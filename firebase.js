55ed0e150d522270fe457ecc84d519fe8eaa8863
suvancha9866 <suvan.chatakondu@gmail.com>
2024-08-04 16:17:58 -0500

    added keys

--- firebase.js
+++ firebase.js
@@ -7,14 +7,15 @@ import {getFirestore} from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyC3s1YmfkDbF5kOOyLWyvjbWkrSY7JyKbw",
+  authDomain: "inventory-management-d2d84.firebaseapp.com",
+  projectId: "inventory-management-d2d84",
+  storageBucket: "inventory-management-d2d84.appspot.com",
+  messagingSenderId: "603234323227",
+  appId: "1:603234323227:web:e21a971e1a1a08a028e1be",
+  measurementId: "G-J69G29TW3H"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
