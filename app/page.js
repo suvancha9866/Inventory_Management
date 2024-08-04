4730b36a4f24f642dd754d30aff88f1765316469
suvancha9866 <suvan.chatakondu@gmail.com>
2024-08-04 14:55:51 -0500

    functionality mostly done

--- app/page.js
+++ app/page.js
@@ -1,95 +1,196 @@
+'use client'
 import Image from "next/image";
-import styles from "./page.module.css";
+import { useState, useEffect } from 'react';
+import { firestore } from '@/firebase';
+import { Box, Button, Modal, Stack, TextField, Typography } from "@mui/material";
+import { collection, doc, getDoc, getDocs, query, setDoc, deleteDoc } from "firebase/firestore";
 
 export default function Home() {
+  const [inventory, setInventory] = useState([]); // This is to set the default value.
+  const [open, setOpen] = useState(false); // Default Value
+  const [itemName, setItemName] = useState(""); // Default Value
+  const [search, setSearch] = useState("");
+
+  // Async won't block code when fetching, that means entire website freezes when fetching
+  const updateInventory = async () => {
+    // this snapshot is the inventory variable in the firestore database
+    // We are querying the collection in our firestore with the name inventory
+    const snapshot = query(collection(firestore, 'inventory'));
+    // This will get all of the docs in the firestore database from the collection inventory as said above
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      });
+    });
+    setInventory(inventoryList);
+  };
+
+  const removeItem = async (item) => {
+    // This gets the direct item reference
+    const docRef = doc(collection(firestore, 'inventory'), item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      if (quantity === 1) {
+        await deleteDoc(docRef);
+      } else {
+        await setDoc(docRef, { quantity: quantity - 1 });
+      }
+    }
+    await updateInventory();
+  };
+
+  const addItem = async (item) => {
+    // This gets the direct item reference
+    const docRef = doc(collection(firestore, 'inventory'), item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      await setDoc(docRef, { quantity: quantity + 1 });
+    } else {
+      await setDoc(docRef, { quantity: 1 });
+    }
+    await updateInventory();
+  };
+
+  // Runs the code in the updateInventory function whenever something in the dependency array changes. 
+  useEffect(() => {
+    updateInventory();
+  }, []);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  // Filter inventory based on search query
+  const filteredItems = inventory.filter((item) =>
+    item.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap={2}
+    >
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <Box
+          position="absolute"
+          top="50%"
+          left="50%"
+          width={400}
+          bgcolor="white"
+          border="2px solid black"
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+          sx={{
+            transform: "translate(-50%,-50%)"
+          }}>
+          <Typography variant="h6">
+            Add Item
+          </Typography>
+          <Stack
+            width="100%"
+            direction="row"
+            spacing={2}>
+            <TextField
+              variant='outlined'
+              fullWidth
+              value={itemName}
+              onChange={(e) => {
+                setItemName(e.target.value);
+              }} />
+            <Button
+              variant="outlined"
+              onClick={() => {
+                addItem(itemName);
+                setItemName("");
+                handleClose();
+              }}>Add</Button>
+          </Stack>
+        </Box>
+      </Modal>
+      <Typography variant="h1">Inventory Management</Typography>
+      <Button
+        variant="contained" onClick={handleOpen}>
+        Add New Item
+      </Button>
+      <Box
+        border="1px solid black">
+        <Box
+          width="800px"
+          height="200px"
+          bgcolor="#ADD8E6"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="column"
+          display="flex">
+          <Typography variant="h2" color="#333">Inventory Items</Typography>
+          <TextField onChange={handleChange} value={search} placeholder="Search Items"></TextField>
+        </Box>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <Stack
+          width="800px"
+          height="300px"
+          spacing={2}
+          overflow="auto">
+          {filteredItems.map(({ name, quantity }) => (
+            <Box
+              key={name}
+              width="100%"
+              minHeight="150px"
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+              bgcolor="#f0f0f0"
+              padding={5}>
+              <Typography variant="h3"
+                color="#333"
+                textAlign="center">
+                {name.charAt(0).toUpperCase() + name.slice(1)}
+              </Typography>
+              <Typography variant="h3"
+                color="#333"
+                textAlign="center">
+                {quantity}
+              </Typography>
+              <Stack
+                direction="row"
+                spacing={2}>
+                <Button variant="contained" onClick={() => {
+                  addItem(name);
+                }}>
+                  Add
+                </Button>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+                <Button variant="contained" onClick={() => {
+                  removeItem(name);
+                }}>
+                  Remove
+                </Button>
+              </Stack>
+            </Box>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          ))}
+        </Stack>
+      </Box>
+    </Box>
   );
 }
---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC3s1YmfkDbF5kOOyLWyvjbWkrSY7JyKbw",
+  authDomain: "inventory-management-d2d84.firebaseapp.com",
+  projectId: "inventory-management-d2d84",
+  storageBucket: "inventory-management-d2d84.appspot.com",
+  messagingSenderId: "603234323227",
+  appId: "1:603234323227:web:e21a971e1a1a08a028e1be",
+  measurementId: "G-J69G29TW3H"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app)
+
+export {firestore}
